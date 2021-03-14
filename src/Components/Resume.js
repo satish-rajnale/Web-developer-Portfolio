a4887fa064410e38e2fba1d59cf728cbfd2eeae0
satish-rajnale <76589507+satish-rajnale@users.noreply.github.com>
2021-03-14 15:24:29 +0530

    second commit

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -16,10 +16,7 @@ class Resume extends Component {
             <p>{work.description}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+    
     }
 
     return (
@@ -41,36 +38,17 @@ class Resume extends Component {
 
 
       <div className="row work">
-
          <div className="three columns header-col">
             <h1><span>Work</span></h1>
          </div>
-
          <div className="nine columns main-col">
           {work}
-        </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
          </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
       </div>
+
+
+
+     
    </section>
     );
   }
